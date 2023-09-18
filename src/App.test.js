8eb55d16b321b116114471eed9c86dea07ce1c7a
Dio <dionatsir@gmail.com>
2023-09-18 13:46:20 +0700

    delete App.test.js

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('render app', () => {
-  render(<App />);
-
-  const linkElement = document.querySelector('a');
-  expect(linkElement).toBeInTheDocument();
-});
--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -1,11 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Home from '.';
 
-test('home js', () => {
+test('home js', async () => {
   render(<Home />, { wrapper: BrowserRouter });
 
   const linkElement = screen.getByText(/go to users page/i);
   expect(linkElement).toBeInTheDocument();
+
+  const goToUser = screen.getByText(/go to users page/i);
+
+  waitFor(() => {
+    fireEvent.click(goToUser);
+
+    expect(screen.getByText(/back to home/i)).toBeInTheDocument();
+  });
 });
